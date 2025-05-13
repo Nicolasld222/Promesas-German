6aa3396b506b2ea33115067b8590f302efd9cf58
Nicolas Londoño Diaz <nicolaslondia@gmail.com>
2025-05-13 18:08:57 -0500

    Revert "Develop"

--- Ejercicio4/Ejercicio4.js
+++
@@ -1,9 +0,0 @@
-//Promise All
-const promesa1 = new Promise((resolve)=> setTimeout(resolve,2000,10));
-const promesa2 = new Promise((resolve)=> setTimeout(resolve,3000,15));
-const promesa3 = new Promise ((resolve)=> setTimeout(resolve,4000,10));
-
-Promise.all([promesa1, promesa2, promesa3])
-.then((resultados)=>{
-    console.log(resultados);
-});
